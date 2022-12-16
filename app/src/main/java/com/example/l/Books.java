a44521f66d6e17712d23af7ff309d6a013b4ae38
rabiagovez <rabiagovez@gmail.com>
2022-12-16 17:11:53 +0300

    getter setter

---
+++ app/src/main/java/com/example/l/Books.java
@@ -0,0 +1,69 @@
+package com.example.l;
+
+public class Books {
+    private String name;
+    private String category;
+    private String writer;
+    private int year;
+    private String publisher;
+    private String ISBN;
+
+    public Books(String name, String category, String writer, int year, String publisher, String ISBN) {
+        this.name = name;
+        this.category = category;
+        this.writer = writer;
+        this.year = year;
+        this.publisher = publisher;
+        this.ISBN = ISBN;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getWriter() {
+        return writer;
+    }
+
+    public void setWriter(String writer) {
+        this.writer = writer;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public String getISBN() {
+        return ISBN;
+    }
+
+    public void setISBN(String ISBN) {
+        this.ISBN = ISBN;
+    }
+
+
+}
